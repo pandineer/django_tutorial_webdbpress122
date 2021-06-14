ec6951d8e98df8e803d856ea067dca5c4ec09409
pandineer <pandineer@gmail.com>
2021-06-14 09:14:46 +0900

    handle 404error

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.template.response import TemplateResponse
 from blog.models import Post
 
@@ -9,7 +10,10 @@ def post_list(request):
   )
 
 def post_detail(request, post_id):
-  post = Post.objects.get(id=post_id)
+  try:
+    post = Post.objects.get(id=post_id)
+  except Post.DoesNotExist:
+    raise Http404
   return TemplateResponse(
     request,
     "post_detail.html",
